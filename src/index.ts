732f3d79fcb28bc9562789311aea522611a2b7b5
george.duan <george.duan@linecorp.com>
2023-02-27 13:03:36 +0800

    initial commits

---
+++ src/index.ts
@@ -0,0 +1,58 @@
+import { Configuration, OpenAIApi } from "openai";
+import * as line from '@line/bot-sdk';
+import * as dotenv from 'dotenv';
+import express from 'express'
+import { WebhookEvent } from "@line/bot-sdk";
+
+
+dotenv.config();
+
+const CHANNEL_ACCESS_TOKEN = process.env.CHANNEL_ACCESS_TOKEN || '';
+const CHANNEL_SECRET = process.env.CHANNEL_SECRET || '';
+const OPENAI_API_KEY = process.env.OPENAI_API_KEY || '';
+const PORT = process.env.PORT || 8080;
+
+const configuration = new Configuration({
+  apiKey: OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+
+
+
+const config = {
+  channelAccessToken: CHANNEL_ACCESS_TOKEN,
+  channelSecret: CHANNEL_SECRET,
+};
+
+const client = new line.Client(config);
+line.middleware(config);
+
+const app = express();
+
+app.post('/webhook', line.middleware(config), (req, res) => {
+  res.status(200).end();
+  Promise.all(req.body.events.map(handleEvent));
+});
+
+async function handleEvent(event: WebhookEvent) {
+  if (event.type !== 'message' || event.message.type !== 'text') {
+    return Promise.resolve(null);
+  }
+  const completion = await openai.createCompletion({
+    model: "text-davinci-003",
+    prompt: event.message.text,
+  });
+
+  return client.replyMessage(event.replyToken, {
+    type: 'text',
+    text: completion.data.choices[0].text || 'not find answer',
+  });
+}
+
+app.get('/', (req, res) => {
+  res.status(200).end('ok');
+})
+app.listen(PORT, () => {
+  console.info(`Service is launched on port: :${PORT}`)
+});
